365277ffb25caba4dc04760d338a526679a18d89
Ian Katz <ifreecarve@gmail.com>
2012-10-30 16:11:32 -0400

    simple stream-grabbing utility. NO SOUND

---
+++ pipe-mplayer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# pipe-mplayer.sh
+#  Uses mplayer to convert a video (especially streaming video) to data on STDOUT
+# by Ian Katz, 2009
+# ifreecarve@gmail.com
+# released under the WTFPL
+
+if [ -n "`ls /tmp/ikmplayer-pipe.* 2>/dev/null`" ]; then
+    rm /tmp/ikmplayer-pipe.*
+fi
+PIPE="/tmp/ikmplayer-pipe.$$"
+#mknod $PIPE p
+mkfifo $PIPE
+
+mencoder "$@" -nosound -really-quiet -ovc copy -o $PIPE  2>/dev/null | cat
+$PIPE 2>/dev/null
+
+rm $PIPE
+#END SCRIPT
+
