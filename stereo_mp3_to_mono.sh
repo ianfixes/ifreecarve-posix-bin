373b152a7c1b443a5ef0796410a7ab78937a134c
Ian Katz <ifreecarve@gmail.com>
2013-09-23 14:37:35 -0400

    os-specific instructions, because OSX's file utility is crippled

--- stereo_mp3_to_mono.sh
+++ stereo_mp3_to_mono.sh
@@ -5,7 +5,20 @@
 # concept via http://superuser.com/a/566023/253931
 #
 
-bitrate=`file "$1" | sed 's/.*, \(.*\)kbps.*/\1/'`
+OS=$(uname)
+OS_X="Darwin"
+OS_LIN="Linux"
+
+
+if [ "$OS_LIN" = "$OS" ] ; then
+    bitrate=`file "$1" | sed 's/.*, \(.*\)kbps.*/\1/'`
+elif [ "$OS_X" = "$OS" ] ; then
+    bitrate=`afinfo "$1" | grep "bits per second" | sed 's/.*: \(.*\)000 bits per second.*/\1/'`
+fi
+
+
+#freq=`afinfo "$1" | grep "Data format" | sed 's/.*, \(.*\)Hz.*/\1/' | awk '{print $1/1000}'`
+
 BR='-V9'
 if [ $bitrate -gt  75 ]; then BR='-V8'; fi
 if [ $bitrate -gt  90 ]; then BR='-V7'; fi
@@ -18,5 +31,6 @@ if [ $bitrate -gt 215 ]; then BR='-V1'; fi
 if [ $bitrate -gt 230 ]; then BR='-V0'; fi
 if [ $bitrate -gt 280 ]; then BR='-b320'; fi
 
-echo "mono-izing file with detected bitrate '$bitrate': $mp3file"
-lame --silent $BR -m m --mp3input "$1" "$2"
+echo "mono-izing file with detected bitrate '$bitrate': $1"
+lame --silent $BR -m m -q 2 --mp3input "$1" "$2"
+#lame --silent $BR --resample $freq -m m -q 2 --mp3input "$1" "$2"
