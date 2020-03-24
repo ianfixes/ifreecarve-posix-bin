eb530a8640c65f76090970e44df9712095cd038c
Ian Katz <ikatz@drizly.com>
2020-03-24 19:59:40 -0400

    A command to open a CWD shell but inside a given docker image

---
+++ docker-image-shell.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# usage function to display help for the hapless user
+usage ()
+{
+     mycmd=`basename $0`
+     echo "$mycmd"
+     echo "usage: $mycmd imagename [shell]"
+     echo "Runs a shell in a docker image with the current directory mounted at /app"
+}
+
+# test if we have an arguments on the command line
+if [ $# -lt 1 ]
+then
+    usage
+    exit
+fi
+
+docker run --rm -it -v "${PWD}:/app" -u root -w /app $@
